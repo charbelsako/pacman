34fb89fb5fec4d2e9136b308ecd5c0f59e0f28b4
charbelsako <charbelsarkis82581@gmail.com>
2018-06-28 13:08:19 +0300

    Fixes issue #17. Added lives

--- Player.js
+++ Player.js
@@ -13,8 +13,17 @@ class Player {
         ellipse(this.x + w/2, this.y + w/2, w - 5)
     }
 
-    scale(){
-        this.i = Math.round(this.x / w)
-        this.j = Math.round(this.y / w)
+    // Wasn't even used.
+    // scale(){
+    //     this.i = Math.round(this.x / w)
+    //     this.j = Math.round(this.y / w)
+    // }
+
+    resetPlayer(){
+        this.i = Math.floor(rows / 2)
+        this.j = cols - 1
+        this.x = this.i * w;
+        this.y = this.j * w;
     }
+    
 }
